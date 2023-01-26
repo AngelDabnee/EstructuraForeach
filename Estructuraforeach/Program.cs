a6b7c8bb81c73691eeee14ff07b7299a0b03df0d
Angel Dabnee Gonzalez Rodriguez <a22311046@uthermosillo.edu.mx>
2023-01-25 23:55:28 -0700

    first commit

---
+++ Estructuraforeach/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace estructuraDeControlforeach// Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {   //La estructura foreach, nos permite recorrer una variable y mostrar sus letras 
+            //o numeros dependiendo de que necesitemos. 
+            Console.WriteLine("Hola mundo");
+            string amigo = "";
+            Console.WriteLine("Captura el nombre de tu amigo");
+            amigo = Console.ReadLine();
+            foreach (char letra in amigo)
+            {   //Imprimimos el amigo que capturaste de manera letra por letra separado por una coma
+                Console.Write(letra + ",");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
